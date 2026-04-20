d066349ac54a628759f234ac7c5b367c43b6aada
jahangiakhatri <jahangirakhatri@gmail.com>
2026-04-20 22:18:44 +0500

    Fix syntax error in Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello from Java inside Docker");
-        System.out.println("Connecting to Database...")
+        System.out.println("Connecting to Database...");
 
     }
 }
